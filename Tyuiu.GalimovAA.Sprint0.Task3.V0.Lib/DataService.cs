aae6b10d8ef912e043e18c4575e6e21478ab2a3f
Tyuiu.GalimovAA@outlook.com <Tyuiu.GalimovAA@outlook.com>
2024-09-18 15:52:14 +0500

    Task3

---
+++ Tyuiu.GalimovAA.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.GalimovAA.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
